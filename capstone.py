9ee45ca730ce571b2a7ebbfb3fe841d4d559b359
RayanIsMe <87342981+RayanIsMe@users.noreply.github.com>
2024-06-29 22:19:10 +0530

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -61,7 +61,7 @@ if st.session_state['SS'] == 1:
                 
                 
                 
-        v1, v2, v3, v4, v5, v6, v7 = st.columns(7)
+        v1, v2, v3, v4, v5 = st.columns(5)
         with v4:
                 if st.button("Start Simulation", key = 15):
                         st.session_state['SS'] = 2
